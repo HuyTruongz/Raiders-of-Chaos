1652410debb610eb385676a757af0ef9b8b813e0
Huy Trương <huytruongth33@gmail.com>
2024-10-20 17:40:00 +0700

    Create DataStruct
    Create DataStruct

---
+++ Assets/RaiderOfChaos/Scripts/Data/DataStruc.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace hyhy.RaidersOfChaos
+{
+    public enum GameTag
+    {
+        Player,
+        Enemy
+    }
+
+    public enum GameScene
+    {
+        Gameplay,
+        MainMenu
+    }
+
+    public enum KeyPref
+    {
+        GameData,
+        IsFirstTime,
+        SpriteOrder
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right, Up, Down
+    }
+
+    public enum GameState
+    {
+        Staring,
+        Playing,
+        Wining,
+        Gameover
+    }
+
+    public enum PlayerState
+    {
+        Idle,
+        Attack,
+        Jump,
+        DoubleJump,
+        Fall,
+        Dead,
+        Ulti,
+        Dash
+    }
+
+    public enum EnemyState
+    {
+        Walk,
+        Idle,
+        Attack,
+        Dead,
+        Def,
+        Hit
+    }
+
+    public enum PlayerCollider
+    {
+        Normal,
+        Dead
+    }
+}
