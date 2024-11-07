fe4d90563912ad28816a99bfbac06f1906b3b94e
HuyTruongz <160222147+HuyTruongz@users.noreply.github.com>
2024-11-07 18:11:17 +0700

    Create Projectile script
    Create Projectile script

--- Assets/RaiderOfChaos/Scripts/Projectile.cs
+++ Assets/RaiderOfChaos/Scripts/Projectile.cs
@@ -6,12 +6,13 @@ using System;
 
 namespace hyhy.RaidersOfChaos
 {
-    public class Projectile : MonoBehaviour/*, IDamageCreater*/
+    public class Projectile : MonoBehaviour, IDamageCreater
     {
         [Header("Base Setting:")]
         public GameTag damageTo;
         public float speed;
         public float damage;
+        public bool deactiveWhenHitted;
 
         [PoolerKeys(target = PoolerTarget.NONE)]
         public string bodyHitPool;
@@ -19,17 +20,69 @@ namespace hyhy.RaidersOfChaos
         [HideInInspector]
         public Actor owner;
 
-        private Vector2 m_lastPos;
+        private Vector2 m_prevPos;
         private RaycastHit2D m_hit;
+        private Vector2 m_dir;
+
+        private void Update()
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime, Space.World);
+        }
+
+        private void FixedUpdate()
+        {
+            DealDamage();
+
+            RefereshLastPos();
+        }
 
         private void OnEnable()
         {
             RefereshLastPos();
         }
 
+        public void DealDamage()
+        {
+            m_dir = (Vector2)transform.position - m_prevPos;
+            float distance = m_dir.magnitude;
+            m_dir.Normalize();
+
+            m_hit = Physics2D.Raycast(m_prevPos, m_dir, distance);
+
+            if(!m_hit && !m_hit.collider) return;
+
+            var col = m_hit.collider;
+
+            Checking(col);
+        }
+
+        private void Checking(Collider2D col)
+        {
+            if (col.CompareTag(damageTo.ToString()))
+            {
+                Actor actor = col.GetComponent<Actor>();
+
+                if(!actor) return;
+
+                actor.WhoHit = owner;
+                actor.TakeDamaged(damage,owner);
+
+                if (deactiveWhenHitted)
+                {
+                    gameObject.SetActive(true);
+                }
+            }
+        }
+
         private void RefereshLastPos()
         {
-           
+            m_prevPos = (Vector2)transform.position;
+        }
+
+        private void OnDisable()
+        {
+            m_hit = new RaycastHit2D();
+            transform.position = new Vector3(1000f,1000f,0);
         }
     }
 
