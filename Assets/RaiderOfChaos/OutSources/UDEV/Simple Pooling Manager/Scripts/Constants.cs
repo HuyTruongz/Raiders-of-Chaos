870cea69a0a68201b6160bf714ddaf9e44c64c60
HuyTruongz <160222147+HuyTruongz@users.noreply.github.com>
2024-11-07 18:45:26 +0700

    Create Pooling Manager
    Create Pooling Manager

--- Assets/RaiderOfChaos/OutSources/UDEV/Simple Pooling Manager/Scripts/Constants.cs
+++ Assets/RaiderOfChaos/OutSources/UDEV/Simple Pooling Manager/Scripts/Constants.cs
@@ -14,7 +14,7 @@
     public static class Constants
     {
         #region EDITOR_SCRIPTS_CONSTANTS
-        public const string PROJECT_FOLDER_NAME = "UDEV/Simple Pooling Manager";
+        public const string PROJECT_FOLDER_NAME = "RaiderOfChaos/UDEV/Simple Pooling Manager";
         public const string EDITOR_DATA_PATH = "Assets/" + PROJECT_FOLDER_NAME + "/Scripts/Editor/Data/";
         #endregion
     }
